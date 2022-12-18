b2b313a7accc736409eabbf106dfc5899dbab493
david-kalachyan <106673743+david-kalachyan@users.noreply.github.com>
2022-12-18 19:15:30 +0400

    The first task is done

---
+++ Part-1/task_1.js
@@ -0,0 +1,19 @@
+/*
+1. Write a program which will print to console N rows of a specific char, like this (char = 'C', N = 3):
+C
+CC
+CCC
+*/
+
+let char = current = "C"
+let count_rows = 100
+
+
+for (let i = 0; i < count_rows; i++) {
+    if (i === 0) {
+        console.log(char)
+    } else {
+        current += char
+        console.log(current)
+    }
+}
---
+++ Part-1/task_2.js
@@ -0,0 +1,14 @@
+/*
+2. Write a program which will print numbers from 1 to N, whith the following conditions: 
+* if a number is divisible by 3, but not divisible by 5, print 'foo' instead of a number
+* if a number is divisible by 5, but not divisible by 3, print 'bar' instead of a number
+* if a number is divisible by both 3 and 5, print 'foobar' instead of number. Example:
+1
+2
+foo
+4
+bar
+6
+...
+*/
+
